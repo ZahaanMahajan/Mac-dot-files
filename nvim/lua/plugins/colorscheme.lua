10ff2faf6481c3b62d047ea23e4de657abce85a0
ZahaanMahajan <zahaanjavaid032@gmail.com>
2026-06-03 11:52:13 +0530

    Ignore git/ and colorls/ config dirs
    Apply Catppuccin Mocha theme overlay on latest config
    
    Rebuilt branch onto latest shared config (superfile, nvim, karabiner,
    tmux, zsh; dropped stale nvim.bak/tmux.bak) and applied Catppuccin
    Mocha: nvim colorscheme, yazi flavor, tmux statusline colors, iterm
    palette, and superfile built-in theme.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,12 @@
 return {
 	{
-		"craftzdog/solarized-osaka.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = true,
 		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
+		opts = {
+			flavour = "mocha",
+			transparent_background = true,
+		},
 	},
 }
-
---- NEOFUSION THEME
--- return { "diegoulloao/neofusion.nvim", priority = 1000, config = true, opts = function() end }
